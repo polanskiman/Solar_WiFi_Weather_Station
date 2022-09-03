8ee0f6afa9e3aa9261bc5ceae9d8b5f30e862fff
polanskiman <polanskiman@users.noreply.github.com>
2022-09-03 19:45:00 +0700

    V2.36 - Updated to work with Blynk Iot (new Blynk)

--- Solar-WiFi-Weather-Station-V2.3 MQTT.ino
+++ Solar-WiFi-Weather-Station-V2.3 MQTT.ino
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------------------------------
-  Project Name : Solar Powered WiFi Weather Station V2.35
+  Project Name : Solar Powered WiFi Weather Station V2.36
   Features: temperature, dewpoint, dewpoint spread, heat index, humidity, absolute pressure, relative pressure, battery status and
   the famous Zambretti Forecaster (multi lingual)
   Authors: Keith Hungerford, Debasish Dutta and Marc Stähli
@@ -62,6 +62,8 @@
   - added August-Roche-Magnus approximation to automatically adjust humidity with temperature corrections
  updated 30/08/22 to v2.35
   -corrected Thingspeak communication issue
+ updated 3/09/22 to v2.36
+  - updated to work with Blynk Iot (new Blynk). Blynk library needs to be updated to latest (v.1.1.0)
   
 ////  Features :  /////////////////////////////////////////////////////////////////////////////////////////////////////////////                                                                                                                   
 // 1. Connect to Wi-Fi, and upload the data to either Blynk App and/or Thingspeak and to any MQTT broker
@@ -77,14 +79,17 @@
  * Enter your personal settings in Settings.h !    *
  *                                                 *
  **************************************************/
+#define BLYNK_PRINT Serial        //This is optional and can be commented if not desired but allows to see library version etc on serial monitor
+#define BLYNK_TEMPLATE_ID ""      //Enter your Blynk Template ID
+#define BLYNK_DEVICE_NAME ""      //Enter your Blynk Device Name
 
 #include "Settings.h"
 #include "Translation.h"
 
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <ESP8266WiFi.h>         //This library needs to be placed before <BlynkSimpleEsp8266.h> else compilation errors occur
 #include <BlynkSimpleEsp8266.h>  //https://github.com/blynkkk/blynk-library
-#include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include "FS.h"
 #include <EasyNTPClient.h>       //https://github.com/aharshac/EasyNTPClient
